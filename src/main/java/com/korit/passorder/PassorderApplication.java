9ddf918c181d5b961a2ee25fc99df523011f3ecc
mini0225 <antcjswo@kakao.com>
2023-02-13 21:59:11 +0900

    23.02.13 - project3

---
+++ src/main/java/com/korit/passorder/PassorderApplication.java
@@ -0,0 +1,13 @@
+package com.korit.passorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korit/passorder/PassorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.passorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
